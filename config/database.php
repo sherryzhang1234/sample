34fd97e317af40b08053cb9d5949a47a1bd0a576
sherryzhang <47157970@qq.com>
2018-01-04 11:35:26 +0000

    updated db config 2

--- config/database.php
+++ config/database.php
@@ -1,5 +1,28 @@
 <?php
 
+//require dirname(__DIR__).'/bootstrap/helpers.php';
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
 $db_config = get_db_config();
 
 return [
